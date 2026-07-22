7c800f67139f93cbcf6dad965d00f8ca994ef9c1
nxp-ran <ran.chen_1@nxp.com>
2026-07-22 17:59:15 +0800

    Fixed the issue that CM7 cannot use littleFS when CM7 XIP from Flash. And added feature that can choose different project based on different link file.

--- bsp/nxp/imx/imxrt/imxrt1180-nxp-evk/cm7/rtconfig.h
+++ bsp/nxp/imx/imxrt/imxrt1180-nxp-evk/cm7/rtconfig.h
@@ -154,6 +154,7 @@
 #define RT_USING_SERIAL
 #define RT_USING_SERIAL_V1
 #define RT_SERIAL_RB_BUFSZ 64
+#define RT_USING_MTD_NOR
 #define RT_USING_PIN
 /* end of Device Drivers */
 
@@ -433,6 +434,7 @@
 
 /* Board extended module Drivers */
 
+#define BSP_LINKER_SCRIPT_RAM
 /* end of Hardware Drivers Config */
 
 #endif
